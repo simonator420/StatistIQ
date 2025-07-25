ac804235dc4d714b8c24b3b014b5aa9398717372
simonator420 <salajsimon@gmail.com>
2025-07-25 22:20:07 +0200

    Initial Commit

---
+++ StatistIQApp/StatistIQAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatistIQAppApp.swift
+//  StatistIQApp
+//
+//  Created by Simon Salaj on 25.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct StatistIQAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StatistIQAppTests/StatistIQAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StatistIQAppTests.swift
+//  StatistIQAppTests
+//
+//  Created by Simon Salaj on 25.07.2025.
+//
+
+import Testing
+@testable import StatistIQApp
+
+struct StatistIQAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
