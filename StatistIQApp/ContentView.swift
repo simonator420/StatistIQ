652336b8b1e53869de92b756b7e37cc523bd985e
simonator420 <salajsimon@gmail.com>
2025-07-30 23:48:39 +0200

    Main folder removed from git

---
+++ StatistIQApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StatistIQApp
+//
+//  Created by Simon Salaj on 25.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    Homepage()
+}
